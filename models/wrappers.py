3c352cf1133ccafe5742f109f77e8775097fd8be
alexferrante <36389437+alexferrante@users.noreply.github.com>
2021-04-28 11:46:05 -0400

    Add custom dataset config

--- models/wrappers.py
+++ models/wrappers.py
@@ -114,6 +114,7 @@ class StyleGAN2(BaseModel):
             'bedrooms': 256,
             'kitchen': 256,
             'places': 256,
+            'dogs-bal': 64,
         }
 
         assert self.outclass in configs, \
@@ -144,7 +145,8 @@ class StyleGAN2(BaseModel):
             'cat': 'https://drive.google.com/uc?export=download&id=15vJP8GDr0FlRYpE8gD7CdeEz2mXrQMgN',
             'places': 'https://drive.google.com/uc?export=download&id=1X8-wIH3aYKjgDZt4KMOtQzN1m4AlCVhm',
             'bedrooms': 'https://drive.google.com/uc?export=download&id=1nZTW7mjazs-qPhkmbsOLLA_6qws-eNQu',
-            'kitchen': 'https://drive.google.com/uc?export=download&id=15dCpnZ1YLAnETAPB0FGmXwdBclbwMEkZ'
+            'kitchen': 'https://drive.google.com/uc?export=download&id=15dCpnZ1YLAnETAPB0FGmXwdBclbwMEkZ',
+            'dogs-bal': 'https://drive.google.com/uc?export=download&id=1poc4iLJstoeS2z40sJr5Q-aZa2rI36LN',
         }
 
         url = checkpoints[self.outclass]
@@ -154,7 +156,7 @@ class StyleGAN2(BaseModel):
         checkpoint_root = os.environ.get('GANCONTROL_CHECKPOINT_DIR', Path(__file__).parent / 'checkpoints')
         checkpoint = Path(checkpoint_root) / f'stylegan2/stylegan2_{self.outclass}_{self.resolution}.pt'
         
-        self.model = stylegan2.Generator(self.resolution, 512, 8).to(self.device)
+        self.model = stylegan2.Generator(self.resolution, 512, 8).to(self.device) # Probably have to change hard-coded 8
 
         if not checkpoint.is_file():
             os.makedirs(checkpoint.parent, exist_ok=True)
